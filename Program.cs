5d74eabb7e35fed3aa76734e7dcb88feba86443f
Lucas Mazo <lucasmazo@gmail.com>
2024-09-09 19:13:00 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "12345", modelo:  "1", imei: "111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Wpp");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "6789", modelo: "2", imei: "222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("telegram");
